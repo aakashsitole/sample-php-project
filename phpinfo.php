6edf451c67e4f64be355ee6d1765fff15cef9ce2
Aakash Sitole <69292310+aakashsitole@users.noreply.github.com>
2021-09-12 00:02:19 +0530

    php info file

---
+++ phpinfo.php
@@ -0,0 +1,22 @@
+<?php 
+// print_r(phpinfo());
+echo "hello text";
+
+// From URL to get webpage contents.
+$url = "https://www.youtube.com/watch?v=_OSCvmUgfXg";
+  
+// Initialize a CURL session.
+$ch = curl_init(); 
+  
+// Return Page contents.
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  
+//grab URL and pass it to the variable.
+curl_setopt($ch, CURLOPT_URL, $url);
+  
+$result = curl_exec($ch);
+  
+echo $result; 
+
+
+?>
